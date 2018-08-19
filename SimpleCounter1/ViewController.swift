0a9d02b72040bc37f53a4fd47628a6a6582403e6
Alim S. Gafar <alimgafar@gmail.com>
2018-08-19 02:48:17 -0400

    Initial Commit

---
+++ SimpleCounter1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCounter1
+//
+//  Created by Alim S. Gafar on 8/19/18.
+//  Copyright © 2018 Alim S. Gafar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
