1e0b0dba2cbff1890201529b1f5224f223145b77
Alim S. Gafar <alimgafar@gmail.com>
2018-08-19 03:28:57 -0400

    First commit.

--- SimpleCounter1/ViewController.swift
+++ SimpleCounter1/ViewController.swift
@@ -9,10 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var resetCountLabel: UIButton!
+    
+    var count:Int?
+    
+    @IBAction func increaseCount(_ sender: Any) {
+        setCount(increase: true);
+        updateDisplay(count: count!);
+    }
+    
+    @IBAction func decreaseCount(_ sender: Any) {
+        setCount(increase: false);
+        updateDisplay(count: count!);
+    }
+    
+    
+    @IBAction func resetCount(_ sender: Any) {
+        count = 0;
+        updateDisplay(count: count!);
+    }
+    
+    func setCount(increase: Bool) {
+        if(increase) {
+            count! += 1;
+        } else if (count != 0) {
+            count! -= 1;
+        }
+    }
+    
+    func updateDisplay(count: Int) {
+        countLabel.text = "\(count)";
+        if(count == 0) {
+            resetCountLabel.isEnabled = false;
+            resetCountLabel.isHidden = true;
+        } else {
+            resetCountLabel.isEnabled = true;
+            resetCountLabel.isHidden = false;
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        count = 0;
     }
 
     override func didReceiveMemoryWarning() {
